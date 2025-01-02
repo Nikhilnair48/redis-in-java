96c1c1bb77f938e1c109d9858d0b897726095761
Nikhilnair48 <nikhil.nair48@gmail.com>
2025-01-02 14:19:28 +0530

    feat: emulator interface updated with persistence related apis

--- src/main/java/api/Emulator.java
+++ src/main/java/api/Emulator.java
@@ -5,4 +5,6 @@ public interface Emulator {
     String get(String key);
     void hset(String hashName, String field, String value, Integer ttl);
     String hget(String key, String field);
+    void saveToFile(String filename);
+    void loadFromFile(String filename);
 }
