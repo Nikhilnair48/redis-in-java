e47d9fd92a056d92c3da95ab6af0f31abd9ae74a
Nikhilnair48 <nikhil.nair48@gmail.com>
2025-01-02 16:20:23 +0530

    feat: adding del to interface

--- src/main/java/api/Emulator.java
+++ src/main/java/api/Emulator.java
@@ -5,6 +5,7 @@ public interface Emulator {
     String get(String key);
     void hset(String hashName, String field, String value, Integer ttl);
     String hget(String key, String field);
+    void del(String key);
     void saveToFile(String filename);
     void loadFromFile(String filename);
 }
